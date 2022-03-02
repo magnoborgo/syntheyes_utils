aac5c11bff5437e636851ec3af9a62ccdc913c17
Magno Borgo <lists@borgo.tv>
2022-03-02 14:07:38 -0500

    fixed selected invisible object crash

--- python/mb_zoom_active_tracker_host.py
+++ python/mb_zoom_active_tracker_host.py
@@ -1,7 +1,8 @@
 # this script will zoom on the active tracker host path (camera/objects)
 # by Magno Borgo 2022
 
-import sys,time
+import sys
+import time
 PY3 = sys.version_info[0] == 3
 
 if PY3:
@@ -15,42 +16,55 @@ hlev.SetView("Quad")
 
 cam = hlev.Scene().activeObj
 
-views = ["top","front","left"]
+views = ["top", "front", "left"]
 
-hlev.Begin() #begin undo action
+hlev.Begin()  # begin undo action
 
 visibleObjList = []
 selectedList = []
+selectedInvisible = []
 mainVisible = True
 
-if cam.show == 0.0: ## if active camera/object is hidden, syntheyes will crash
-    cam.Set("show",1.0)
+if cam.show == 0.0:  # if active camera/object is hidden, syntheyes will crash
+    cam.Set("show", 1.0)
     mainVisible = False
+
 
 for t in hlev.Trackers():
     if t.Get("isShown") and t.isSelected == 0.0:
-        t.Set("isShown",0.0)
+        t.Set("isShown", 0.0)
         visibleObjList.append(t)
     if t.isSelected == 1.0:
         selectedList.append(t)
+        if t.Get("isShown") == 0.0:
+            t.Set("isShown", 1.0)
+            selectedInvisible.append(t)
+
 
 for m in hlev.Meshes():
     if m.show == 1.0 and m.isSelected == 0.0:
-        m.Set("show",0.0)
+        m.Set("show", 0.0)
         visibleObjList.append(m)
     if m.isSelected == 1.0:
         selectedList.append(m)
+        if m.show == 0.0:
+            m.Set("show", 1.0)
+            selectedInvisible.append(m)
+
 for o in hlev.Objects():
     if o.show == 1.0 and o != cam:
-        o.Set("show",0.0)
+        o.Set("show", 0.0)
         visibleObjList.append(o)
-    if o.isSelected == 1.0:
+    if o.isSelected == 1.0 and o != cam:
         selectedList.append(o)
+        if o.show == 0.0:
+            o.Set("show", 1.0)
+            selectedInvisible.append(o)
 
-if len(selectedList) > 0: #objects selected, hide the cam/obj
+if len(selectedList) > 0:  # there are objects selected, then hide the cam/obj
     for o in hlev.Objects():
         if o.show == 1.0 and o == cam:
-            o.Set("show",0.0)
+            o.Set("show", 0.0)
             visibleObjList.append(o)
 
 hlev.Accept("Hide all visible scene objects except the camera")
@@ -61,10 +75,17 @@ for v in views:
 hlev.Begin()
 for t in visibleObjList:
     if t.Type() in ["TRK"]:
-        t.Set("isShown",1.0)
+        t.Set("isShown", 1.0)
     else:
-        t.Set("show",1)
+        t.Set("show", 1.0)
+
+for t in selectedInvisible:
+    if t.Type() in ["TRK"]:
+        t.Set("isShown", 0.0)
+    else:
+        t.Set("show", 0.0)
+
 if not mainVisible:
-    cam.Set("show",0.0)
+    cam.Set("show", 0.0)
 
 hlev.Accept("Show all previous visible scene objects")
